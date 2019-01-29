48278bf04f18ec8b5882c33ed222c4ad5a899657
nicolas <nraby@emi.u-bordeaux1.fr>
2019-01-29 13:52:13 +0100

    Javadoc fonctionnelle, script installation fonctionnel

--- install.sh
+++ install.sh
@@ -7,11 +7,12 @@ source /espace/Auber_PLE-005/user-env.sh
 echo __Maven installation__
 mvn install
 
-echo __Generating the JavaDoc__
-##cd  ./src/main/java
+echo __Generating the Javadoc__
+javadoc -d ./src/main/java/bigdata/Javadoc/ ./src/main/java/bigdata/*.java
 
 echo Please go here to check the documentation :
-
+echo /BigDataMaps/src/main/java/bigdata/Javadoc
+echo Or use the script ./jd 
 
 echo __Spark__
 spark-submit --master yarn --deploy-mode cluster --class bigdata.ProjetMaps --executor-memory 5GB --num-executors 14 --executor-cores 3 ./target/ProjetMaps-0.0.1.jar 
