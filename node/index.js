681535c682f35632786a1ce87384ee59d59b7b52
Carlos Alberto Villavicencio Avila <cvillavicenc@emi.u-bordeaux1.fr>
2019-01-29 11:04:32 +0100

    read image

--- node/index.js
+++ node/index.js
@@ -1,3 +1,4 @@
+
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -5,17 +6,22 @@ const hbaseDB = require('hbase-rpc-client');
 const app = express();
 
 const http = require('http');
-const DBHOST = process.env.DBHOST || 'localhost:2181';
+const DBHOST = process.env.DBHOST || 'young:2181';
 const DBROOT = process.env.DBROOT || '/hbase';
-const DBNAME = 'HBASENAME';
+//const DBNAME = 'villavicencio';
 
-/*const client = hbaseDB({
+const client = hbaseDB({
   zookeeperHosts: [ DBHOST ],
   zookeeperRoot: DBROOT,
   zookeeperReconnectTimeout: 20000,
   rpcTimeout: 30000,
-});*/
+});
 
+Number.prototype.pad = function(size) {
+    var s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+}
 
 http.createServer(function(req, res) {
 
@@ -24,20 +30,72 @@ http.createServer(function(req, res) {
     res.end('Salut tout le monde !');
   
   });
+
 app.use(bodyParser.json());
 
-app.use('/img/:x/:y/:z', (req, res, next) => {
-  // Afficher une image se trouvant dans HBase
-});
+
+app.get('/:x/:y/:z.png', function (req, res){
+	var x = parseInt(req.params.x);
+	var y = parseInt(req.params.y);
+	var z = parseInt(req.params.z);
+	
+	var col;
+	var lig;
+
+	if (x != 8){
+		res.writeHead(404, {"Content-Type": "text/plain"});
+  		 res.write("404 Not Found\n");
+  		 res.end();
+		 return;	
+	}
+
+	if (y>=0 && y<=179){
+		var column = 180 - y;
+		col = "W"+(column).pad(3);
+	}
+	else if (y>=180 && y<=359){
+		var column = y-180;
+		col = "E"+(column).pad(3);
+	}else{
+		res.writeHead(404, {"Content-Type": "text/plain"});
+  		 res.write("404 Not Found\n");
+  		 res.end();
+		 return;
+	}
+
+	
+	if (z>=0 && z<=89){
+		var column = 89 - z;
+		lig = "N"+(column).pad(2);
+	}
+	else if (z>=90 && z<=179){
+		var column = z-89;
+		lig = "S"+(column).pad(2);
+	}else{
+		 res.writeHead(404, {"Content-Type": "text/plain"});
+  		 res.write("404 Not Found\n");
+  		 res.end();
+		 return;
+	}
+
+	var get = new hbaseDB.Get(lig+col);
+	client.get("villavicencio", get, function (err, row) {
+		res.writeHead(200, {'Content-Type':'image/png'});
+		res.end(row.cols['hgt_data:image'].value.toString('base64'),'Base64');
+	});
+})
 
 app.get('/', function (req, res) {
 
-    res.send('BigData')
+
+
+    res.send('BigData');
 })
 
 app.listen(8000, function () {
     console.log('Example app listening on port 8000!')
   })
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
